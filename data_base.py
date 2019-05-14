a553daacb986b022391323459946de7de6a7d1fc
Zaicol <42048262+Zaicol@users.noreply.github.com>
2019-05-14 15:03:47 +0300

    New data_base ver.

--- data_base.py
+++ data_base.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vwb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///UnderTask.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -14,8 +13,10 @@ class TasksModel(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     desc = db.Column(db.String(1000), unique=True, nullable=False)
     author = db.Column(db.Integer, unique=False, nullable=False)
+    user = db.Column(db.Integer, unique=False, default=0)
     date = db.Column(db.DateTime, unique=False, nullable=False)
     priority = db.Column(db.Integer, unique=False, nullable=False, default=1)
+    done_date = db.Column(db.DateTime, unique=False, nullable=True)
     phase = db.Column(db.Integer, unique=False, nullable=False, default=0)
 
 
@@ -25,18 +26,8 @@ class UsersModel(db.Model):
     password_hash = db.Column(db.String(128), unique=False, nullable=False)
     donetasks = db.Column(db.String(), unique=False,
                           nullable=True, default='[]')
-    alice_id = db.Column(db.String(), unique=False, default='')
+    alice_id = db.Column(db.String(), unique=False, default=0)
     tg_id = db.Column(db.Integer, unique=False, default=0)
 
 
-if __name__ == '__main__':
-    db.drop_all()
-    db.create_all()
-    user = UsersModel(name='admin', password_hash='admin')
-    user1 = UsersModel(name='user1', password_hash='user1')
-    task = TasksModel(name='Это имя', desc='Это описание', author=1, date=datetime.now())
-    db.session.add(user)
-    db.session.add(user1)
-    db.session.add(task)
-    db.session.commit()
-    app.run()
+db.create_all()
