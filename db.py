e5142ad285cb8c04816385692d4face600e2767a
Ilya <ilyaily@gmail.com>
2019-05-14 10:41:44 +0300

    Database classes created

---
+++ db.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+class TasksModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    desc = db.Column(db.String(1000), unique=True, nullable=False)
+    author = db.Column(db.Integer, unique=False, nullable=False)
+    user = db.Column(db.Integer, unique=False, nullable=False)
+    date = db.Column(db.DateTime, unique=False, nullable=False)
+    phase = db.Column(db.Integer, unique=False, nullable=False, default=0)
+
+
+class UsersModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), unique=False, nullable=False)
+    donetasks = db.Column(db.String(), unique=False,
+                          nullable=True, default='[]')
